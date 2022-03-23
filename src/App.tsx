246d562034d50e7bd43b29bf15216fb80d9e3f92
Peter Parker <web3slinger@gmail.com>
2022-03-23 20:18:16 +0800

    Add search animal function

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,52 @@
-import { useState } from 'react'
-import logo from './logo.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  interface Test {
+    id: number
+    type: string
+    name: string
+    age: number
+  }
+  const [animals, setAnimals] = useState<Test[]>([])
 
-  return <div></div>
+  useEffect(() => {
+    const lastQuery = localStorage.getItem('lastQuery')
+    search(lastQuery)
+  }, [])
+
+  const search = async (q: any) => {
+    const response = await fetch(
+      'http://localhost:8080?' + new URLSearchParams({ q })
+    )
+    const data = await response.json()
+    setAnimals(data)
+
+    localStorage.setItem('lastQuery', q)
+  }
+
+  return (
+    <main>
+      <h1>Animal Farm</h1>
+
+      <input
+        type="text"
+        placeholder="Search"
+        onChange={(e) => search(e.target.value)}
+      />
+
+      <ul>
+        {animals.map((animal) => (
+          <li key={animal.id}>
+            <strong>{animal.type}</strong> {animal.name} ({animal.age} years
+            old)
+          </li>
+        ))}
+
+        {animals.length === 0 && 'No animals found'}
+      </ul>
+    </main>
+  )
 }
 
 export default App
